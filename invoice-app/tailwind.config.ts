47b8c603450089bff4f78308fd9bd9ec9bf1b086
Mariia Ushakova <m.ushakova22@gmail.com>
2024-02-20 15:40:38 +0100

    feat: add colors and fonts

--- invoice-app/tailwind.config.ts
+++ invoice-app/tailwind.config.ts
@@ -7,6 +7,22 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
+    colors: {
+      text: "#FFFFFF",
+      darkText: "#141625",
+      background: "#F8F8FB",
+      headerBackground: "#252945",
+      primary: "#7C5DFA",
+      primaryPale: "#9277FF",
+      error: "#EC5757",
+      errorPale: "#FF9797",
+      secondary: "#7E88C3",
+      secondaryPale: "#DFE3FA",
+      secondaryDark: "#888EB0",
+      paid: "#33D69F",
+      pending: "#FF8F00",
+      draft: "#373B53"
+    },
     extend: {}
   },
   plugins: [],

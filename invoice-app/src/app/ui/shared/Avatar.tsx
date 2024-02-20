8aad48e4e5f8eec9a018ed00964f552c39a96b29
Mariia Ushakova <m.ushakova22@gmail.com>
2024-02-20 21:07:34 +0100

    fix: header

--- invoice-app/src/app/ui/shared/Avatar.tsx
+++ invoice-app/src/app/ui/shared/Avatar.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Avatar() {
   return (
-    <div className="flex items-center justify-center h-18 w-18 border-l border-borderColor">
-      <Image src="/assets/image-avatar.jpg" width={32} height={32} alt="Avatar" className="rounded-full" />
+    <div className="flex items-center justify-center h-[4.5rem] w-[4.5rem] border-l border-borderColor">
+      <Image src="/assets/image-avatar.jpg" width={32} height={32} alt="Avatar" className="rounded-full hover:cursor-pointer" />
     </div>)
 }

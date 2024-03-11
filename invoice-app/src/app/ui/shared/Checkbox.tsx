691fc11540e15cd4140fcdaf31b44e512ca46cae
Mariia Ushakova <m.ushakova22@gmail.com>
2024-03-11 20:47:49 +0100

    style: checkbox label

--- invoice-app/src/app/ui/shared/Checkbox.tsx
+++ invoice-app/src/app/ui/shared/Checkbox.tsx
@@ -9,7 +9,7 @@ type CheckboxProps = {
 
 export default function Checkbox({ id, checked, name, onChange }: CheckboxProps) {
   return (
-    <div className="w-full flex gap-2">
+    <div className="w-full flex flex-row items-center gap-2">
       <input type="checkbox" id={id} name={name} checked={checked} onChange={onChange} className="relative peer shrink-0 appearance-none w-4 h-4 border-2 border-primary rounded-sm bg-secondaryPale dark:bg-cardColor mt-1 checked:bg-primary dark:checked:bg-primary checked:border-0 outline-none focus:outline-none" />
       <svg className="absolute w-4 h-4 mt-1 hidden peer-checked:block pointer-events-none"
         xmlns="http://www.w3.org/2000/svg"
@@ -19,7 +19,7 @@ export default function Checkbox({ id, checked, name, onChange }: CheckboxProps)
         stroke-width="4">
         <path d="M1.5 4.49976L3.62425 6.62402L8.96995 1.27832" stroke="white" strokeWidth="2" />
       </svg>
-      <label htmlFor={id} className="font-bold cursor-pointer">{name}</label>
+      <label htmlFor={id} className="w-4 h-4 font-bold cursor-pointer">{name}</label>
     </div>
   );
 }
